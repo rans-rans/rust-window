be95ee1ad7f60d8f8f8b3beae69acdf9bd9d4a2f
rans-rans <ransfordowusuansah9@gmail.com>
2026-04-03 10:52:05 +0000

    enable closing the ram window when main window close

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,14 +2,24 @@ use sysinfo::System;
 
 mod ram;
 use ram::listen_to_ram;
-use tauri::{AppHandle, WebviewUrl, WebviewWindowBuilder};
+use tauri::{AppHandle, Manager, WebviewUrl, WebviewWindowBuilder, WindowEvent};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        // Register the commands that can be invoked from the frontend.
         .invoke_handler(tauri::generate_handler![get_system_info, open_ram_window])
+        // Listen to RAM updates in the background and send them to the frontend when they occur.
         .setup(listen_to_ram)
+        // Ensure that when the main window is closed, the RAM details window is also closed if it's open.
+        .on_window_event(|window, event| {
+            if window.label() == "main" && matches!(event, WindowEvent::CloseRequested { .. }) {
+                if let Some(ram_window) = window.app_handle().get_webview_window("ram-details") {
+                    let _ = ram_window.close();
+                }
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -27,8 +37,7 @@ fn get_system_info() -> OsInfo {
 
 #[tauri::command]
 fn open_ram_window(app: AppHandle) {
-    let _ram_window =
-        WebviewWindowBuilder::new(&app, "ram-details", WebviewUrl::App("ram-info".into()))
+    let _ = WebviewWindowBuilder::new(&app, "ram-details", WebviewUrl::App("ram-info".into()))
             .title("RAM Details")
             .inner_size(600.0, 400.0)
             .build();
