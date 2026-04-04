d238d8fbafd501a3ae015d3f963131a748e8a5dc
rans-rans <ransfordowusuansah9@gmail.com>
2026-04-04 20:15:08 +0000

    added confirm to closing window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,20 +3,32 @@ use sysinfo::System;
 mod ram;
 use ram::listen_to_ram;
 use tauri::{AppHandle, Manager, WebviewUrl, WebviewWindowBuilder, WindowEvent};
+use tauri_plugin_dialog::{MessageDialogButtons, MessageDialogKind};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         // Register the commands that can be invoked from the frontend.
         .invoke_handler(tauri::generate_handler![get_system_info, open_ram_window])
         // Listen to RAM updates in the background and send them to the frontend when they occur.
         .setup(listen_to_ram)
-        // Ensure that when the main window is closed, the RAM details window is also closed if it's open.
         .on_window_event(|window, event| {
-            if window.label() == "main" && matches!(event, WindowEvent::CloseRequested { .. }) {
-                if let Some(ram_window) = window.app_handle().get_webview_window("ram-details") {
-                    let _ = ram_window.close();
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                if window.label() == "main" {
+                    api.prevent_close();
+                    let app = window.app_handle().clone();
+                    let _ = tauri_plugin_dialog::DialogExt::dialog(&app)
+                        .message("Are you sure you want to close this window?")
+                        .title("Confirm Close")
+                        .kind(MessageDialogKind::Warning)
+                        .buttons(MessageDialogButtons::OkCancel)
+                        .show( move |result| {
+                            if result {
+                                app.exit(0);
+                            }
+                        });
                 }
             }
         })
@@ -38,9 +50,9 @@ fn get_system_info() -> OsInfo {
 #[tauri::command]
 fn open_ram_window(app: AppHandle) {
     let _ = WebviewWindowBuilder::new(&app, "ram-details", WebviewUrl::App("ram-info".into()))
-            .title("RAM Details")
-            .inner_size(600.0, 400.0)
-            .build();
+        .title("RAM Details")
+        .inner_size(600.0, 400.0)
+        .build();
 }
 
 #[derive(Clone, serde::Serialize, Debug)]
